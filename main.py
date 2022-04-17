eb50d6157d106054c2c6f0a53060ab7a46003ead
Grigor Radkov <grigorrradkov@gmail.com>
2022-04-17 19:24:09 +0300

    Initial drawing of board

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from cmath import rect
+
+from matplotlib.pyplot import bar
 import pygame as pg
 import engine
 
@@ -8,17 +11,22 @@ MAX_FPS = 60
 IMAGES = {}
 PIECES = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
 
+# rgb
+white_color = (255, 255, 255)
+black_color = (0, 0, 0)
+gray_color = (192, 192, 192)
+
 pg.init()
 
 def load_images():
     for piece in PIECES:
-        IMAGES[piece] = pg.transform.scale(pg.image.load("images/{piece}.png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = pg.transform.scale(pg.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
 def main():
     load_images()
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     clock = pg.time.Clock()
-    screen.fill(pg.Color("green"))
+    screen.fill(pg.Color(gray_color))
     gameState = engine.GameState()
     print(gameState.board)
     running = True
@@ -26,9 +34,27 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-
+        drawGameState(screen, gameState)
         clock.tick(MAX_FPS)
         pg.display.flip()
+
+def drawGameState(screen, gs):
+    drawBaseBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBaseBoard(screen):
+
+    for row in range(BOARD_DIM):
+        for col in range(BOARD_DIM):      
+            alternate_color = gray_color if((row + col) % 2) else white_color
+            pg.draw.rect(screen, alternate_color, pg.Rect((col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE)))
+
+def drawPieces(screen, board):
+    for row in range(BOARD_DIM):
+        for col in range(BOARD_DIM):
+            piece = board[row][col]
+            if piece != "--" :
+                screen.blit(IMAGES[piece], pg.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 if __name__ == "__main__":
     main()
