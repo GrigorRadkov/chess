eba986ed1f15e54d1572798b6a05e5410a867307
Grigor Radkov <grigorrradkov@gmail.com>
2023-04-02 18:50:39 +0300

    Helper functions template class

---
+++ movementCheckerFunctions.py
@@ -0,0 +1,16 @@
+def checkColUpwards(row, col, turn, board):
+    pass
+def checkColDownwards(row, col, turn, board):
+    pass
+def checkRowRight(row, col, turn, board):
+    pass
+def checkRowLeft(row, col, turn, board):
+    pass
+def checkDiagonalRightAndUp(row, col, turn, board):
+    pass
+def checkDiagonalRightAndDown(row, col, turn, board):
+    pass
+def checkDiagonalLeftAndUp(row, col, turn, board):
+    pass
+def checkDiagonalLeftAndDown(row, col, turn, board):
+    pass
